123ecf8b90f1ab3665f3fcaedd8fc2e11023667a
Ed Emmett <edmund.emmett@renishaw.com>
2025-02-18 14:25:41 +0000

    Starting headerfile

--- FITS.c/LCD.c
+++ FITS.c/LCD.c
@@ -118,14 +118,13 @@
 
 
 void showChar(char c, int position);
-void showCharB(char c, int position);
 #define pos1 4                                                 // Digit A1 - L4
 #define pos2 6                                                 // Digit A2 - L6
 #define pos3 8                                                 // Digit A3 - L8
 #define pos4 10                                                // Digit A4 - L10
 #define pos5 2                                                 // Digit A5 - L2
 #define pos6 18                                                // Digit A6 - L18
-#define pos7 1                                                // Digit A6 - L18
+
 // Define word access definitions to LCD memories
 #define LCDMEMW ((int*)LCDMEM)
 #define LCDBMEMW ((int*)LCDBMEM)
---
+++ FITS.c/LCD.h
@@ -0,0 +1,2 @@
+void setAlarm();
+void clearAlarm();

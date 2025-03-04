ab09ed314a1391d113a25436dbf783088a2ef0cb
AM151316 <alex.murray@renishaw.com>
2025-03-04 15:44:07 +0000

    Flashing default value added.

--- FITS.c/alarm.c
+++ FITS.c/alarm.c
@@ -1,13 +1,34 @@
+#include "intrinsics.h"
 void alarm () {
     //do something
+    // Define an enum for the alarm process states
+typedef enum {
+    MINUTES,
+    HOURS
+} AlarmProcessStates;
+AlarmProcessStates currentState = MINUTES;
+int i = 0;
+
 while(1){
-        int i = 0;
-    for(i;i<30000;i++){
-        if(i==29999){
-                LCD_WriteAll('A', 'L', 'A', 'R', 'M');
-                LCD_WriteSingle(' ', 6);
-                i = 0;
+            switch (currentState) {
+            case MINUTES: // flash the minutes section on LCD
+                __delay_cycles(150000);
+                LCD_WriteAll('1', '2', '0', '0', ' ');
+                // Show semicolon needed
+                // show two full stops needed
+                __delay_cycles(150000);
+                LCD_WriteAll('1', '2', ' ', ' ', ' ');
+                // Show semicolon needed
+                // show two full stops needed
+
+                // if start/stop button pressed, increment digits
+                //if lap/reset button pressed move to hours state
+            break;
+            case HOURS:
+            // if start/stop button pressed, increment digits
+                //if lap/reset button pressed move to minutes state
+            break;
         }
-    }
+
 }
     }

668808d37f535aedc2ae7cea5b6527508cc69521
AM151316 <alex.murray@renishaw.com>
2025-03-18 13:07:29 +0000

    Commented alarm.h and alarm.c

--- FITS.c/alarm.h
+++ FITS.c/alarm.h
@@ -1,6 +1,6 @@
-#include <msp430.h>
-#include "LCD.h"
-void alarm();
+#include <msp430.h> // generic board control library
+#include "LCD.h"    // for controlling the onboard LCD
+void alarm();       // main loop for the alarm configuration mode of the stop watch
 
-int Alarm_getAlarmMins(); // returns the current mins value for the alarm
-int Alarm_getAlarmHours(); // returns the current mins value for the alarm
+int Alarm_getAlarmMins();   // returns the current mins value for the alarm
+int Alarm_getAlarmHours();  // returns the current hours value for the alarm

465d0b531e75c610fc0828afe09075cdb120be0c
AlexPegg <alex@alexpegg.uk>
2025-03-18 15:04:51 +0000

    Update chrono.c

--- FITS.c/chrono.c
+++ FITS.c/chrono.c
@@ -25,7 +25,7 @@ void chrono () {
             case IDLE:
                 //if button pressed move to start state
                 LCD_WriteAll('1','0','0','0','0', '0');
-                LCD_setChrono();
+                LCD_setStopWatch();
                 if (getSTARTSTOP()) {
                     state = START;
                     clearSTARTSTOP();
@@ -38,10 +38,7 @@ void chrono () {
                 break;
             case ACTIVE:
             {
-                // timer is counting
-                // int time = TA0CCR0 - startTime;
                 uint16_t time = getGlobalTimer() - startTime;
-                // Calculate minutes, seconds, and centiseconds
                 // Calculate minutes, seconds, and centiseconds
                 int minutes = time / 6000;               // 1 minute = 6000 centiseconds
                 int seconds = (time % 6000) / 100;       // 1 second = 100 centiseconds
@@ -54,7 +51,7 @@ void chrono () {
                 digits[2] = seconds / 10;       // First digit of seconds
                 digits[3] = seconds % 10;       // Second digit of seconds
                 digits[4] = centiseconds / 10;  // First digit of centiseconds
-                digits[5] = centiseconds % 10;
+                digits[5] = centiseconds % 10;  // Second ditig of centiseconds
                 LCD_WriteAll(digits[0], digits[1], digits[2], digits[3], digits[4], digits[5]);
                 LCD_setColon();
                 LCD_setDecimalTwo();

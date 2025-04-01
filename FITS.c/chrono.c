3ab41aadf58022de2dfeb50682e394cfd0a7360c
AlexPegg <alex@alexpegg.uk>
2025-04-01 14:47:03 +0100

    implemented lapping and reset

--- FITS.c/chrono.c
+++ FITS.c/chrono.c
@@ -7,6 +7,7 @@
 #define START 1
 #define ACTIVE 2
 #define STOPPED 3
+#define LAPPED 4
 
 
 void chrono () {
@@ -17,6 +18,8 @@ void chrono () {
 
     int state = IDLE;
     int startTime = 0;
+    int lappedTime = 0;
+    int lastResetButtonState = 0;
     
     //when start is pressed, 
 
@@ -24,7 +27,7 @@ void chrono () {
         switch (state) {
             case IDLE:
                 //if button pressed move to start state
-                LCD_WriteAll('1','0','0','0','0', '0');
+                LCD_WriteAll('0','0','0','0','0', '0');
                 LCD_setStopWatch();
                 if (getSTARTSTOP()) {
                     state = START;
@@ -60,10 +63,66 @@ void chrono () {
                     state = STOPPED;
                     clearSTARTSTOP();
                 }
+                if (!(P1IN & BIT4) && lastResetButtonState == 0) { // check if lap/reset button pressed
+                    __delay_cycles(150000); // wait
+                    if (!(P1IN & BIT4)) { // Check again if switch is still pressed for software debouncing
+                        lappedTime = getGlobalTimer();
+                        state = LAPPED;
+                        lastResetButtonState = 1;
+                    } 
+                }
+                lastResetButtonState = !(P1IN & BIT4);
+                break;
+            }
+            case LAPPED:
+            {
+                //display the most recent lap time,
+                uint16_t time = lappedTime - startTime;
+                // Calculate minutes, seconds, and centiseconds
+                int minutes = time / 6000;               // 1 minute = 6000 centiseconds
+                int seconds = (time % 6000) / 100;       // 1 second = 100 centiseconds
+                int centiseconds = time % 100;
+
+                char digits[6];
+                // Extract digits for mm:ss:SS format
+                digits[0] = minutes / 10;       // First digit of minutes
+                digits[1] = minutes % 10;       // Second digit of minutes
+                digits[2] = seconds / 10;       // First digit of seconds
+                digits[3] = seconds % 10;       // Second digit of seconds
+                digits[4] = centiseconds / 10;  // First digit of centiseconds
+                digits[5] = centiseconds % 10;  // Second ditig of centiseconds
+                LCD_WriteAll(digits[0], digits[1], digits[2], digits[3], digits[4], digits[5]);
+                LCD_setColon();
+                LCD_setDecimalTwo();
+                //if button is pressed, upate most recent time.
+                if (!(P1IN & BIT4) && lastResetButtonState == 0) { // check if lap/reset button pressed
+                    __delay_cycles(150000); // wait
+                    if (!(P1IN & BIT4)) { // Check again if switch is still pressed for software debouncing
+                        lappedTime = getGlobalTimer();
+                        state = LAPPED;
+                        lastResetButtonState = 1;
+                    } 
+                }
+                lastResetButtonState = !(P1IN & BIT4);
+                
+                if (getSTARTSTOP()) {
+                    state = STOPPED;
+                    clearSTARTSTOP();
+                }
                 break;
             }
             case STOPPED:
                 //reset.
+                if (!(P1IN & BIT4) && lastResetButtonState == 0) { // check if lap/reset button pressed
+                    __delay_cycles(150000); // wait
+                    if (!(P1IN & BIT4)) { // Check again if switch is still pressed for software debouncing
+                        lappedTime = 0;
+                        startTime = 0;
+                        state = IDLE;
+                        lastResetButtonState = 1;
+                    } 
+                }
+                lastResetButtonState = !(P1IN & BIT4);
                 break;
         }
     }
